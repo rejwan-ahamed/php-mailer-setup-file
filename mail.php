7f0bb578b45fc1634838d9163c6ed583d9955cfe
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-07-07 20:03:27 +0600

    first

---
+++ mail.php
@@ -0,0 +1,56 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+
+       function smtpmailer($to, $from, $from_name, $subject, $body)
+    {
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true; 
+ 
+        $mail->SMTPSecure = 'ssl'; 
+        $mail->Host = 'Your domain name';
+        $mail->Port = 'your SMTP port';  
+        $mail->Username = 'username';
+        $mail->Password = 'password';   
+   
+   //   $path = 'reseller.pdf';
+   //   $mail->AddAttachment($path);
+   
+        $mail->IsHTML(true);
+        $mail->From="info@fastpaybd.com";
+        $mail->FromName=$from_name;
+        $mail->Sender=$from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AddAddress($to);
+        if(!$mail->Send())
+        {
+            $error ="Please try Later, Error Occured while Processing...";
+            return $error; 
+        }
+        else 
+        {
+            $error = "Thanks You !! Your email is sent.";  
+            return $error;
+        }
+    }
+    
+    $to   = 'recipient email id';
+    $from = 'your email id';
+    $name = 'Fast Pay BD';
+    $subj = 'subject';
+    $msg = 'message';
+    
+    $error=smtpmailer($to,$from, $name ,$subj, $msg);
+
+
+?>
